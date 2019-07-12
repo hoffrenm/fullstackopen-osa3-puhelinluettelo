0cf8b099954e00c6a4ac677d6c5655ad79f1d164
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-07-12 12:44:24 +0300

    buildui

---
+++ build/precache-manifest.8a0aebbfc5e352c627092c724dd9b257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7c13875fed8d5e7714bd0fdea117d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14506b65d091e08c362f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9c2daf6e6642a0c8030d",
+    "url": "/static/js/2.5c99a4f3.chunk.js"
+  },
+  {
+    "revision": "14506b65d091e08c362f",
+    "url": "/static/js/main.b9bd0028.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
